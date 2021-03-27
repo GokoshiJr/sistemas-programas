9f9b42cce299a68fb5e2edc8c7340b2e7c78be04
Julio Gonzalez <julioagonzalez18@gmail.com>
2021-03-27 17:34:22 -0400

    fix: Add path in navigation and connect bd local

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,26 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../database");
 
-router.get("/", async (req, res) => {
-  res.render("home");
+router.get("/", async(req, res) => {
+  res.redirect("/home")
+});
+
+router.get("/home", async (req, res) => {
+
+  const rutas_home = [
+    {nombre: "Ruta1", ruta: "/#"},
+    {nombre: "Ruta2", ruta: "/#"},
+    {nombre: "Ruta3", ruta: "/#"},
+    {nombre: "Ruta4", ruta: "/#"}
+  ];
+
+  res.render("home", { rutas_home });
+
   /* 
   const data = await pool.query("SELECT * FROM estatus");
   console.log(data); 
   */
+
 });
 
 module.exports = router;
