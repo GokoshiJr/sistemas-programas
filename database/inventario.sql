ea91f3f814e1ce27196dc6cdfe01edff2f9b8f95
Julio Gonzalez <julioagonzalez18@gmail.com>
2021-03-31 13:41:36 -0400

    feat: Update of BD Tables

--- database/inventario.sql
+++ database/inventario.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:4000
--- Generation Time: Mar 09, 2021 at 07:06 PM
+-- Generation Time: Mar 31, 2021 at 05:39 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.2.19
 
@@ -25,15 +25,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `administradores`
+--
+
+CREATE TABLE `administradores` (
+  `administrador_id` int(10) UNSIGNED NOT NULL,
+  `empleado_id` int(10) UNSIGNED NOT NULL,
+  `estatus_id` int(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `almacenistas`
+--
+
+CREATE TABLE `almacenistas` (
+  `almacenista_id` int(10) UNSIGNED NOT NULL,
+  `empleado_id` int(10) UNSIGNED NOT NULL,
+  `estatus_id` int(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `empleados`
 --
 
 CREATE TABLE `empleados` (
   `empleado_id` int(10) UNSIGNED NOT NULL,
-  `estatus_id` int(10) UNSIGNED NOT NULL,
-  `jerarquia_id` int(10) UNSIGNED NOT NULL,
+  `estatus_id` int(1) UNSIGNED NOT NULL,
   `nombre` varchar(50) NOT NULL,
-  `apellido` varchar(100) NOT NULL
+  `apellido` varchar(100) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `cedula` int(10) UNSIGNED NOT NULL,
+  `telefono` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -43,7 +69,7 @@ CREATE TABLE `empleados` (
 --
 
 CREATE TABLE `estatus` (
-  `estatus_id` int(10) UNSIGNED NOT NULL,
+  `estatus_id` int(1) UNSIGNED NOT NULL,
   `nombre` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -58,34 +84,114 @@ INSERT INTO `estatus` (`estatus_id`, `nombre`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `jerarquias`
+-- Table structure for table `instrucciones`
 --
 
-CREATE TABLE `jerarquias` (
-  `jerarquia_id` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(100) NOT NULL
+CREATE TABLE `instrucciones` (
+  `instruccion_id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `estatus_id` int(1) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `jerarquias`
+-- Table structure for table `productos`
 --
 
-INSERT INTO `jerarquias` (`jerarquia_id`, `nombre`) VALUES
-(1, 'Almacenista'),
-(2, 'Supervisor'),
-(3, 'Administrador');
+CREATE TABLE `productos` (
+  `producto_id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `codigo` int(10) UNSIGNED NOT NULL,
+  `cantidad` int(10) UNSIGNED NOT NULL,
+  `tipoproducto_id` int(10) UNSIGNED NOT NULL,
+  `estatus_id` int(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registros`
+--
+
+CREATE TABLE `registros` (
+  `registro_id` int(10) UNSIGNED NOT NULL,
+  `codigo` int(10) UNSIGNED NOT NULL,
+  `producto_id` int(10) UNSIGNED NOT NULL,
+  `instruccion_id` int(10) UNSIGNED NOT NULL,
+  `fecha` date NOT NULL,
+  `cantidad_producto` int(10) UNSIGNED NOT NULL,
+  `estatus_id` int(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supervisores`
+--
+
+CREATE TABLE `supervisores` (
+  `supervisor_id` int(10) UNSIGNED NOT NULL,
+  `empleado_id` int(10) UNSIGNED NOT NULL,
+  `estatus_id` int(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tipoproductos`
+--
+
+CREATE TABLE `tipoproductos` (
+  `tipoproducto_id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `estatus_id` int(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `usuario_id` int(10) UNSIGNED NOT NULL,
+  `empleado_id` int(10) UNSIGNED NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `clave` varchar(100) NOT NULL,
+  `fecha_registro` timestamp NOT NULL,
+  `ultima_conexion` timestamp NOT NULL,
+  `estatus_id` int(1) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `administradores`
+--
+ALTER TABLE `administradores`
+  ADD PRIMARY KEY (`administrador_id`),
+  ADD KEY `administradores_empleados_fk` (`empleado_id`),
+  ADD KEY `estatus_administradores_fk` (`estatus_id`);
+
+--
+-- Indexes for table `almacenistas`
+--
+ALTER TABLE `almacenistas`
+  ADD PRIMARY KEY (`almacenista_id`),
+  ADD KEY `almacenistas_empleados_fk` (`empleado_id`),
+  ADD KEY `almacenistas_estatus_pk` (`estatus_id`);
+
+--
 -- Indexes for table `empleados`
 --
 ALTER TABLE `empleados`
   ADD PRIMARY KEY (`empleado_id`),
-  ADD KEY `empleados_estatus_fk` (`estatus_id`),
-  ADD KEY `empleados_jerarquias_fk` (`jerarquia_id`);
+  ADD UNIQUE KEY `cedula` (`cedula`),
+  ADD UNIQUE KEY `telefono` (`telefono`),
+  ADD KEY `empleados_estatus_fk` (`estatus_id`);
 
 --
 -- Indexes for table `estatus`
@@ -94,14 +200,67 @@ ALTER TABLE `estatus`
   ADD PRIMARY KEY (`estatus_id`);
 
 --
--- Indexes for table `jerarquias`
+-- Indexes for table `instrucciones`
 --
-ALTER TABLE `jerarquias`
-  ADD PRIMARY KEY (`jerarquia_id`);
+ALTER TABLE `instrucciones`
+  ADD PRIMARY KEY (`instruccion_id`),
+  ADD KEY `instrucciones_estatus_fk` (`estatus_id`);
+
+--
+-- Indexes for table `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`producto_id`),
+  ADD KEY `productos_tipoproductos_fk` (`tipoproducto_id`),
+  ADD KEY `productos_estatus_fk` (`estatus_id`);
+
+--
+-- Indexes for table `registros`
+--
+ALTER TABLE `registros`
+  ADD PRIMARY KEY (`registro_id`),
+  ADD KEY `registros_productos_fk` (`producto_id`),
+  ADD KEY `registros_instrucciones_fk` (`instruccion_id`),
+  ADD KEY `registros_estatus_fk` (`estatus_id`);
+
+--
+-- Indexes for table `supervisores`
+--
+ALTER TABLE `supervisores`
+  ADD PRIMARY KEY (`supervisor_id`),
+  ADD KEY `supervisores_empleados_fk` (`empleado_id`),
+  ADD KEY `supervisores_estatus_fk` (`estatus_id`);
+
+--
+-- Indexes for table `tipoproductos`
+--
+ALTER TABLE `tipoproductos`
+  ADD PRIMARY KEY (`tipoproducto_id`),
+  ADD KEY `tipoproductos_estatus_fk` (`estatus_id`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`usuario_id`),
+  ADD KEY `usuarios_empleados_fk` (`empleado_id`),
+  ADD KEY `usuarios_estatus_fk` (`estatus_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `administradores`
+--
+ALTER TABLE `administradores`
+  MODIFY `administrador_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `almacenistas`
+--
+ALTER TABLE `almacenistas`
+  MODIFY `almacenista_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `empleados`
@@ -113,24 +272,108 @@ ALTER TABLE `empleados`
 -- AUTO_INCREMENT for table `estatus`
 --
 ALTER TABLE `estatus`
-  MODIFY `estatus_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `estatus_id` int(1) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `jerarquias`
+-- AUTO_INCREMENT for table `instrucciones`
 --
-ALTER TABLE `jerarquias`
-  MODIFY `jerarquia_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `instrucciones`
+  MODIFY `instruccion_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `producto_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `registros`
+--
+ALTER TABLE `registros`
+  MODIFY `registro_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `supervisores`
+--
+ALTER TABLE `supervisores`
+  MODIFY `supervisor_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tipoproductos`
+--
+ALTER TABLE `tipoproductos`
+  MODIFY `tipoproducto_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `usuario_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `administradores`
+--
+ALTER TABLE `administradores`
+  ADD CONSTRAINT `administradores_empleados_fk` FOREIGN KEY (`empleado_id`) REFERENCES `empleados` (`empleado_id`),
+  ADD CONSTRAINT `estatus_administradores_fk` FOREIGN KEY (`estatus_id`) REFERENCES `estatus` (`estatus_id`);
+
+--
+-- Constraints for table `almacenistas`
+--
+ALTER TABLE `almacenistas`
+  ADD CONSTRAINT `almacenistas_empleados_fk` FOREIGN KEY (`empleado_id`) REFERENCES `empleados` (`empleado_id`),
+  ADD CONSTRAINT `almacenistas_estatus_pk` FOREIGN KEY (`estatus_id`) REFERENCES `estatus` (`estatus_id`);
+
+--
 -- Constraints for table `empleados`
 --
 ALTER TABLE `empleados`
-  ADD CONSTRAINT `empleados_estatus_fk` FOREIGN KEY (`estatus_id`) REFERENCES `estatus` (`estatus_id`),
-  ADD CONSTRAINT `empleados_jerarquias_fk` FOREIGN KEY (`jerarquia_id`) REFERENCES `jerarquias` (`jerarquia_id`);
+  ADD CONSTRAINT `empleados_estatus_fk` FOREIGN KEY (`estatus_id`) REFERENCES `estatus` (`estatus_id`);
+
+--
+-- Constraints for table `instrucciones`
+--
+ALTER TABLE `instrucciones`
+  ADD CONSTRAINT `instrucciones_estatus_fk` FOREIGN KEY (`estatus_id`) REFERENCES `estatus` (`estatus_id`);
+
+--
+-- Constraints for table `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_estatus_fk` FOREIGN KEY (`estatus_id`) REFERENCES `estatus` (`estatus_id`),
+  ADD CONSTRAINT `productos_tipoproductos_fk` FOREIGN KEY (`tipoproducto_id`) REFERENCES `tipoproductos` (`tipoproducto_id`);
+
+--
+-- Constraints for table `registros`
+--
+ALTER TABLE `registros`
+  ADD CONSTRAINT `registros_estatus_fk` FOREIGN KEY (`estatus_id`) REFERENCES `estatus` (`estatus_id`),
+  ADD CONSTRAINT `registros_instrucciones_fk` FOREIGN KEY (`instruccion_id`) REFERENCES `instrucciones` (`instruccion_id`),
+  ADD CONSTRAINT `registros_productos_fk` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`producto_id`);
+
+--
+-- Constraints for table `supervisores`
+--
+ALTER TABLE `supervisores`
+  ADD CONSTRAINT `supervisores_empleados_fk` FOREIGN KEY (`empleado_id`) REFERENCES `empleados` (`empleado_id`),
+  ADD CONSTRAINT `supervisores_estatus_fk` FOREIGN KEY (`estatus_id`) REFERENCES `estatus` (`estatus_id`);
+
+--
+-- Constraints for table `tipoproductos`
+--
+ALTER TABLE `tipoproductos`
+  ADD CONSTRAINT `tipoproductos_estatus_fk` FOREIGN KEY (`estatus_id`) REFERENCES `estatus` (`estatus_id`);
+
+--
+-- Constraints for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_empleados_fk` FOREIGN KEY (`empleado_id`) REFERENCES `empleados` (`empleado_id`),
+  ADD CONSTRAINT `usuarios_estatus_fk` FOREIGN KEY (`estatus_id`) REFERENCES `estatus` (`estatus_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
