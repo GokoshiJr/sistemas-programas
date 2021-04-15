a4cc211a4b78392b2438a87616a5e32afefd85d6
Marcel Gonzalez <marcel202101@gmail.com>
2021-04-15 01:56:24 -0400

    Insercion de Productos

--- database/inventario.sql
+++ database/inventario.sql
@@ -1,339 +1,177 @@
--- phpMyAdmin SQL Dump
--- version 5.0.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:4000
--- Generation Time: Apr 15, 2021 at 12:49 AM
--- Server version: 5.7.24
--- PHP Version: 7.2.19
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `db_sistema`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `almacenes`
---
-
-CREATE TABLE `almacenes` (
-  `almacen_id` int(10) UNSIGNED NOT NULL,
-  `sector` varchar(10) CHARACTER SET utf8 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `almacenes`
---
-
-INSERT INTO `almacenes` (`almacen_id`, `sector`) VALUES
-(1, 'Norte'),
-(2, 'Sur');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cargos`
---
-
-CREATE TABLE `cargos` (
-  `cargo_id` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(20) CHARACTER SET utf8 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `cargos`
---
-
-INSERT INTO `cargos` (`cargo_id`, `nombre`) VALUES
-(1, 'Administrador'),
-(2, 'Supervisor'),
-(3, 'Almacenista');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `empleados`
---
-
-CREATE TABLE `empleados` (
-  `empleado_id` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `apellido` varchar(100) NOT NULL,
-  `fecha_nacimiento` date NOT NULL,
-  `cedula` int(10) UNSIGNED NOT NULL,
-  `telefono` int(10) UNSIGNED NOT NULL,
-  `cargo_id` int(10) UNSIGNED NOT NULL,
-  `almacen_id` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `empleados`
---
-
-INSERT INTO `empleados` (`empleado_id`, `nombre`, `apellido`, `fecha_nacimiento`, `cedula`, `telefono`, `cargo_id`, `almacen_id`) VALUES
-(1, 'Julio', 'Gonzalez', '2000-12-12', 28195303, 4247300796, 1, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `estatus`
---
-
-CREATE TABLE `estatus` (
-  `estatus_id` int(1) UNSIGNED NOT NULL,
-  `nombre` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `instrucciones`
---
-
-CREATE TABLE `instrucciones` (
-  `instruccion_id` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `productos`
---
-
-CREATE TABLE `productos` (
-  `producto_id` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `codigo` int(10) UNSIGNED NOT NULL,
-  `cantidad` int(10) UNSIGNED NOT NULL,
-  `tipoproducto_id` int(10) UNSIGNED NOT NULL,
-  `almacen_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `registros`
---
-
-CREATE TABLE `registros` (
-  `registro_id` int(10) UNSIGNED NOT NULL,
-  `codigo` int(10) UNSIGNED NOT NULL,
-  `producto_id` int(10) UNSIGNED NOT NULL,
-  `instruccion_id` int(10) UNSIGNED NOT NULL,
-  `fecha` date NOT NULL,
-  `cantidad_producto` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tipoproductos`
---
-
-CREATE TABLE `tipoproductos` (
-  `tipoproducto_id` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `estatus_id` int(1) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `usuario_id` int(10) UNSIGNED NOT NULL,
-  `empleado_id` int(10) UNSIGNED NOT NULL,
-  `correo` varchar(100) NOT NULL,
-  `clave` varchar(100) NOT NULL,
-  `fecha_registro` timestamp NOT NULL,
-  `ultima_conexion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `usuarios`
---
-
-INSERT INTO `usuarios` (`usuario_id`, `empleado_id`, `correo`, `clave`, `fecha_registro`, `ultima_conexion`) VALUES
-(1, 1, 'julioagonzalez18@gmail.com', '123', '2021-04-14 21:47:55', '2021-04-14 22:22:21');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `almacenes`
---
-ALTER TABLE `almacenes`
-  ADD PRIMARY KEY (`almacen_id`);
-
---
--- Indexes for table `cargos`
---
-ALTER TABLE `cargos`
-  ADD PRIMARY KEY (`cargo_id`);
-
---
--- Indexes for table `empleados`
---
-ALTER TABLE `empleados`
-  ADD PRIMARY KEY (`empleado_id`),
-  ADD UNIQUE KEY `cedula` (`cedula`),
-  ADD UNIQUE KEY `telefono` (`telefono`),
-  ADD KEY `empleados_cargos_fk` (`cargo_id`),
-  ADD KEY `empleados_almacenes_fk` (`almacen_id`);
-
---
--- Indexes for table `estatus`
---
-ALTER TABLE `estatus`
-  ADD PRIMARY KEY (`estatus_id`);
-
---
--- Indexes for table `instrucciones`
---
-ALTER TABLE `instrucciones`
-  ADD PRIMARY KEY (`instruccion_id`);
-
---
--- Indexes for table `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`producto_id`),
-  ADD KEY `productos_tipoproductos_fk` (`tipoproducto_id`);
-
---
--- Indexes for table `registros`
---
-ALTER TABLE `registros`
-  ADD PRIMARY KEY (`registro_id`),
-  ADD KEY `registros_productos_fk` (`producto_id`),
-  ADD KEY `registros_instrucciones_fk` (`instruccion_id`);
-
---
--- Indexes for table `tipoproductos`
---
-ALTER TABLE `tipoproductos`
-  ADD PRIMARY KEY (`tipoproducto_id`),
-  ADD KEY `tipoproductos_estatus_fk` (`estatus_id`);
-
---
--- Indexes for table `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`usuario_id`),
-  ADD UNIQUE KEY `correo` (`correo`),
-  ADD KEY `usuarios_empleados_fk` (`empleado_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `almacenes`
---
-ALTER TABLE `almacenes`
-  MODIFY `almacen_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `cargos`
---
-ALTER TABLE `cargos`
-  MODIFY `cargo_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `empleados`
---
-ALTER TABLE `empleados`
-  MODIFY `empleado_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `estatus`
---
-ALTER TABLE `estatus`
-  MODIFY `estatus_id` int(1) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `instrucciones`
---
-ALTER TABLE `instrucciones`
-  MODIFY `instruccion_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `productos`
---
-ALTER TABLE `productos`
-  MODIFY `producto_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `registros`
---
-ALTER TABLE `registros`
-  MODIFY `registro_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tipoproductos`
---
-ALTER TABLE `tipoproductos`
-  MODIFY `tipoproducto_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `usuario_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `empleados`
---
-ALTER TABLE `empleados`
-  ADD CONSTRAINT `empleados_almacenes_fk` FOREIGN KEY (`almacen_id`) REFERENCES `almacenes` (`almacen_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `empleados_cargos_fk` FOREIGN KEY (`cargo_id`) REFERENCES `cargos` (`cargo_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `productos`
---
-ALTER TABLE `productos`
-  ADD CONSTRAINT `productos_tipoproductos_fk` FOREIGN KEY (`tipoproducto_id`) REFERENCES `tipoproductos` (`tipoproducto_id`);
-
---
--- Constraints for table `registros`
---
-ALTER TABLE `registros`
-  ADD CONSTRAINT `registros_instrucciones_fk` FOREIGN KEY (`instruccion_id`) REFERENCES `instrucciones` (`instruccion_id`),
-  ADD CONSTRAINT `registros_productos_fk` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`producto_id`);
-
---
--- Constraints for table `tipoproductos`
---
-ALTER TABLE `tipoproductos`
-  ADD CONSTRAINT `tipoproductos_estatus_fk` FOREIGN KEY (`estatus_id`) REFERENCES `estatus` (`estatus_id`);
-
---
--- Constraints for table `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_empleados_fk` FOREIGN KEY (`empleado_id`) REFERENCES `empleados` (`empleado_id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- MySQL Script generated by MySQL Workbench
+-- 04/15/21 01:52:58
+-- Model: New Model    Version: 1.0
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema db_sistema
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema db_sistema
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `db_sistema` DEFAULT CHARACTER SET latin1 ;
+USE `db_sistema` ;
+
+-- -----------------------------------------------------
+-- Table `db_sistema`.`almacenes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_sistema`.`almacenes` (
+  `almacen_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `sector` VARCHAR(10) CHARACTER SET 'utf8' NOT NULL,
+  PRIMARY KEY (`almacen_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = latin1;
+
+-- -----------------------------------------------------
+-- Table `db_sistema`.`cargos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_sistema`.`cargos` (
+  `cargo_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(20) CHARACTER SET 'utf8' NOT NULL,
+  PRIMARY KEY (`cargo_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = latin1;
+
+-- -----------------------------------------------------
+-- Table `db_sistema`.`empleados`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_sistema`.`empleados` (
+  `empleado_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(50) NOT NULL,
+  `apellido` VARCHAR(100) NOT NULL,
+  `fecha_nacimiento` DATE NOT NULL,
+  `cedula` INT(10) UNSIGNED NOT NULL,
+  `telefono` INT(10) UNSIGNED NOT NULL,
+  `cargo_id` INT(10) UNSIGNED NOT NULL,
+  `almacen_id` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`empleado_id`),
+  UNIQUE INDEX `cedula` (`cedula` ASC),
+  UNIQUE INDEX `telefono` (`telefono` ASC),
+  INDEX `empleados_cargos_fk` (`cargo_id` ASC),
+  INDEX `empleados_almacenes_fk` (`almacen_id` ASC),
+  CONSTRAINT `empleados_almacenes_fk`
+    FOREIGN KEY (`almacen_id`)
+    REFERENCES `db_sistema`.`almacenes` (`almacen_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `empleados_cargos_fk`
+    FOREIGN KEY (`cargo_id`)
+    REFERENCES `db_sistema`.`cargos` (`cargo_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
+
+-- -----------------------------------------------------
+-- Table `db_sistema`.`estatus`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_sistema`.`estatus` (
+  `estatus_id` INT(1) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`estatus_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
+
+-- -----------------------------------------------------
+-- Table `db_sistema`.`instrucciones`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_sistema`.`instrucciones` (
+  `instruccion_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`instruccion_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8;
+
+-- -----------------------------------------------------
+-- Table `db_sistema`.`tipoproductos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_sistema`.`tipoproductos` (
+  `tipoproducto_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(100) NOT NULL,
+  `estatus_id` INT(1) UNSIGNED NOT NULL,
+  PRIMARY KEY (`tipoproducto_id`),
+  INDEX `tipoproductos_estatus_fk` (`estatus_id` ASC),
+  CONSTRAINT `tipoproductos_estatus_fk`
+    FOREIGN KEY (`estatus_id`)
+    REFERENCES `db_sistema`.`estatus` (`estatus_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 16
+DEFAULT CHARACTER SET = utf8;
+
+-- -----------------------------------------------------
+-- Table `db_sistema`.`productos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_sistema`.`productos` (
+  `producto_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(100) NOT NULL,
+  `codigo` INT(10) UNSIGNED NOT NULL,
+  `cantidad` INT(10) UNSIGNED NOT NULL,
+  `tipoproducto_id` INT(10) UNSIGNED NOT NULL,
+  `almacen_id` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`producto_id`),
+  INDEX `productos_tipoproductos_fk` (`tipoproducto_id` ASC),
+  CONSTRAINT `productos_tipoproductos_fk`
+    FOREIGN KEY (`tipoproducto_id`)
+    REFERENCES `db_sistema`.`tipoproductos` (`tipoproducto_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 38
+DEFAULT CHARACTER SET = utf8;
+
+-- -----------------------------------------------------
+-- Table `db_sistema`.`registros`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_sistema`.`registros` (
+  `registro_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `codigo` INT(10) UNSIGNED NOT NULL,
+  `producto_id` INT(10) UNSIGNED NOT NULL,
+  `instruccion_id` INT(10) UNSIGNED NOT NULL,
+  `fecha` DATE NOT NULL,
+  `cantidad_producto` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`registro_id`),
+  INDEX `registros_productos_fk` (`producto_id` ASC),
+  INDEX `registros_instrucciones_fk` (`instruccion_id` ASC),
+  CONSTRAINT `registros_instrucciones_fk`
+    FOREIGN KEY (`instruccion_id`)
+    REFERENCES `db_sistema`.`instrucciones` (`instruccion_id`),
+  CONSTRAINT `registros_productos_fk`
+    FOREIGN KEY (`producto_id`)
+    REFERENCES `db_sistema`.`productos` (`producto_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+-- -----------------------------------------------------
+-- Table `db_sistema`.`usuarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_sistema`.`usuarios` (
+  `usuario_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `empleado_id` INT(10) UNSIGNED NOT NULL,
+  `correo` VARCHAR(100) NOT NULL,
+  `clave` VARCHAR(100) NOT NULL,
+  `fecha_registro` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `ultima_conexion` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`usuario_id`),
+  UNIQUE INDEX `correo` (`correo` ASC),
+  INDEX `usuarios_empleados_fk` (`empleado_id` ASC),
+  CONSTRAINT `usuarios_empleados_fk`
+    FOREIGN KEY (`empleado_id`)
+    REFERENCES `db_sistema`.`empleados` (`empleado_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
