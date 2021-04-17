7ef60e041af17cd03ac443dbec48d5cd9fecf48b
Marcel Gonzalez <marcel202101@gmail.com>
2021-04-16 20:20:37 -0400

    Se redirecciono el home a login

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,24 +2,8 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../database");
 
-router.get("/", async(req, res) => {
-  res.redirect("/home")
-});
-
-router.get("/home", async (req, res) => {
-  
-  let ruta_login = {
-    nombre: "Login",
-    ruta: "/users/login"
-  };
-
-  const rutas_contacto = [
-    {nombre: "twitter", ruta: "https://twitter.com/GokoshiJr"},
-    {nombre: "github", ruta: "https://github.com/GokoshiJr/sistemas-programas"}
-  ];
-
-  res.render("home", { ruta_login, rutas_contacto });
-
+router.get("/home", async(req, res) => {
+  res.redirect("/users/login")
 });
 
 module.exports = router;
